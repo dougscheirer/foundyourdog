617342d6bb9ee476367d883cc595beea051bbfd9
Doug Scheirer <dscheirer@perforce.com>
2017-01-01 16:05:25 -0800

    shuffle the home page a little

--- nodejs/src/app/components/home.js
+++ nodejs/src/app/components/home.js
@@ -7,22 +7,18 @@ const Home = React.createClass({
     const style = "jumbotron homemain";
     return (
       <div className={style}>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <Link to="found"><img width="100%" src="/img/found.jpg" alt="found a dog"></img>
-                  <p>I found a dog</p>
-                </Link>
-              </td>
-              <td>
-                <Link to="lost"><img width="100%" src="/img/lost.jpg" alt="lost a dog"></img>
-                  <p>I lost a dog</p>
-                </Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="container">
+          <div className="col-md-6">
+            <Link to="found"><img width="100%" src="/img/found.jpg" alt="found a dog"></img>
+              <p>I found a dog</p>
+            </Link>
+          </div>
+          <div className="col-md-6">
+            <Link to="lost"><img width="100%" src="/img/lost.jpg" alt="lost a dog"></img>
+              <p>I lost a dog</p>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
