da93a70b64bba4d849e7102c4a70c6fb7cee7aba
dscheirer <dscheirer@perforce.com>
2017-01-09 13:24:05 -0800

    light improvements to the migration runner

--- src/main/java/com/foundyourdog/app/db/FlywayMigration.java
+++ src/main/java/com/foundyourdog/app/db/FlywayMigration.java
@@ -1,5 +1,8 @@
 package com.foundyourdog.app.db;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.flywaydb.core.Flyway;
 
 public class FlywayMigration {
@@ -7,19 +10,23 @@ public class FlywayMigration {
       Flyway flyway = new Flyway();
       
       flyway.setDataSource(DBConnection.getUrl(), DBConnection.getUser(), DBConnection.getPassword());
-      String action = (args.length == 0) ? "migrate" : args[0];
-      switch (action) {
-        case "clean":
-          flyway.clean();
-          break;
-        case "migrate":
-          flyway.migrate();
-          break;
-        case "repair":
-            flyway.repair();
-            break;
-        default:
-          System.out.println("No flyway action");
+      String[] defaultActions = new String[] { "migrate" };
+      String[] actions = (args.length == 0) ? defaultActions : args;
+      for (String action : actions) {
+	      switch (action) {
+	        case "clean":
+	          flyway.clean();
+	          break;
+	        case "migrate":
+	          flyway.migrate();
+	          break;
+	        case "repair":
+	          flyway.repair();
+	          break;
+	        default:
+	          System.out.println("SKIP: No flyway action for " + action);
+	          break;
+	      }
       }
     }
 }
